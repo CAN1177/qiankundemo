62b937be8d1e0d5c202381f3c3544781d234d8f8
Silva <yican1175@163.com>
2022-11-16 20:45:59 +0800

    fix: 完善主应用

--- src/main.tsx
+++ src/main.tsx
@@ -2,27 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
-import { registerMicroApps, start } from "qiankun";
+import "./registerApps";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
-
-registerMicroApps([
-  {
-    name: "react app", // app name registered
-    entry: "//localhost:7100",
-    container: "#yourContainer",
-    activeRule: "/yourActiveRule",
-  },
-  {
-    name: "vue app",
-    entry: { scripts: ["//localhost:7100/main.js"] },
-    container: "#yourContainer2",
-    activeRule: "/yourActiveRule2",
-  },
-]);
-
-start();
